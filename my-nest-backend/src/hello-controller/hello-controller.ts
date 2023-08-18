6d705cdf1cdbcd915b1d4bd78cdf11a4e9aab9cc
Soo <soojlee0106@naver.com>
2023-08-19 00:16:25 +0900

    Set up post in nest

--- my-nest-backend/src/hello-controller/hello-controller.ts
+++ my-nest-backend/src/hello-controller/hello-controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('your-endpoint')
 export class HelloController {
-  @Get()
-  getHello(): string {
-    return JSON.stringify({ message: 'Hello from your backend!' });
+  @Post()
+  postHello(): string {
+    return JSON.stringify({ message: 'Reply from your backend!' });
   }
 }
