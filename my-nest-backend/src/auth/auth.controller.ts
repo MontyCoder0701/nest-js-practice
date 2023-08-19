38d1ce84a61bddabbf00ec3e6e2bef9152bbc110
Soo <soojlee0106@naver.com>
2023-08-19 11:50:59 +0900

    Add dto and validation

--- my-nest-backend/src/auth/auth.controller.ts
+++ my-nest-backend/src/auth/auth.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Post, Body, BadRequestException } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import * as bcrypt from 'bcrypt';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller()
 export class AuthController {
@@ -8,7 +10,14 @@ export class AuthController {
     }
 
     @Post('register')
-    register() {
-        return 'This is a registration route';
+    async register(@Body() body: RegisterDto) {
+
+        if (body.password !== body.password_confirm) {
+            throw new BadRequestException('Password does not match!');
+        }
+
+        const hashed = await bcrypt.hash(body.password, 12);
+        body.password = hashed;
+        return this.authService.create(body);
     }
 }
